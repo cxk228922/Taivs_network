c7a6de405f03eadf6ce317f32595cce733494adc
cxk228922 <ben042708@gmail.com>
2024-11-12 17:21:05 +0800

    Update unit08 and unit09 homework files

--- unit09/hw01.php
+++ unit09/hw01.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>資訊二甲05江寬泓</title>
+    <title>資訊二甲05江寬泓 9-1</title>
     <style>
         body {
             background-color: <?php echo (date("s") % 2 == 0) ? "#ADD8E6" : "#FAD6D6"; ?>;
--- unit09/hw02.php
+++ unit09/hw02.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>資訊二甲05江寬泓</title>
+    <title>資訊二甲05江寬泓 9-2</title>
     <style>
         .color-0 { color: red; }
         .color-1 { color: orange; }

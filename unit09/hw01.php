48083fbd0886fe2e7018578c76e608a6a3d3d59c
cxk228922 <ben042708@gmail.com>
2024-11-12 22:45:38 +0800

    Initial commit

---
+++ unit09/hw01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>資訊二甲05江寬泓</title>
+    <style>
+        body {
+            background-color: <?php echo (date("s") % 2 == 0) ? "#ADD8E6" : "#FAD6D6"; ?>;
+        }
+    </style>
+</head>
+<body>
+    <p></p>
+</body>
+</html>
---
+++ unit09/hw02.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>資訊二甲05江寬泓</title>
+    <style>
+        .color-0 { color: red; }
+        .color-1 { color: orange; }
+        .color-2 { color: yellow; }
+        .color-3 { color: green; }
+        .color-4 { color: blue; }
+        .color-5 { color: indigo; }
+        .color-6 { color: purple; }
+    </style>
+</head>
+<body>
+    <?php
+        for($i = 1;$i<=100;$i+=1){
+            $Color_class = "color-".($i%7-1);
+            echo "<p class='$Color_class'> number{$i}</p>" ;
+        }
+    ?>
+</body>
+</html>

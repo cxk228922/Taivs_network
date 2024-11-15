e5ce12ccc2346810f5cea3f952a0274842a9cc24
cxk228922 <ben042708@gmail.com>
2024-11-15 11:07:20 +0800

    unit10-hw02

--- unit10/hw02.php
+++ unit10/hw02.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap">
     <title>Document</title>
     <style>
         body {
@@ -51,14 +54,20 @@
             background-color: #45a049;
         }
 
-        
     </style>
 </head>
 <body>
     <?php
-        $name = $_POST['userInput'];
-        echo "Your Input: $name<br>";
-        echo "string length : ".strlen($name);
+        $a = $_POST['a'];
+        $b = $_POST['b'];
+        $c = $_POST['c'];
+        echo '\( a \) + \( b \) = '.($a+ $b).'<br>';
+        echo '\( a \) - \( b \) = '.($a- $b).'<br>';
+        echo '\( a \) * \( b \) = '.($a* $b).'<br>';
+        echo '\( a \) / \( b \) = '.($a/ $b).'<br>';
+        echo '\( a \) % \( c \) = '.($a% $c).'<br>';
+        echo '\( a \) + \( 1 \) = '.($a+ 1).'<br>';
+        echo '\( c \) - \( 1 \) = '.($c- 1).'<br>';
     ?>
 </body>
 </html>

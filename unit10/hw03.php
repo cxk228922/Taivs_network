0c441a5dfc9123b65fcc1dcae011ecc9dc8d39cb
Robin <150093538+cxk228922@users.noreply.github.com>
2024-11-29 11:47:40 +0800

    Update hw03.php

--- unit10/hw03.php
+++ unit10/hw03.php
@@ -59,34 +59,56 @@
         $password = $_POST['passwd'];
         $ID = $_POST['id'];
 
-        function checkTaiwanID($id) {
-            if (!preg_match("/^[A-Z][1-2][0-9]{8}$/", $id)) {
-                return false;
-            }
-        
-            $letters = [
-                'A' => 10, 'B' => 11, 'C' => 12, 'D' => 13, 'E' => 14,
-                'F' => 15, 'G' => 16, 'H' => 17, 'I' => 34, 'J' => 18,
-                'K' => 19, 'L' => 20, 'M' => 21, 'N' => 22, 'O' => 35,
-                'P' => 23, 'Q' => 24, 'R' => 25, 'S' => 26, 'T' => 27,
-                'U' => 28, 'V' => 29, 'W' => 32, 'X' => 30, 'Y' => 31, 'Z' => 33
-            ];
-        
-            $letter = substr($id, 0, 1);
-            $number = substr($id, 1);
-        
-            $letterValue = $letters[$letter];
-            $sum = intval($letterValue / 10) + ($letterValue % 10) * 9;
-        
-            for ($i = 1; $i <= 8; $i++) {
-                $sum += $number[$i - 1] * (9 - $i);
-            }
-        
-            $sum += $number[8];
-        
-            return $sum % 10 === 0;
-        }
-        if(!checkTaiwanID($ID)){
+        function validateTaiwanID($id) {
+    // 檢查長度是否為10
+    if (strlen($id) != 10) {
+        return false;
+    }
+
+    $id = strtoupper($id); // 轉換為大寫
+
+    // 使用正則表達式檢查格式
+    if (!preg_match("/^[A-Z][12][0-9]{8}$/", $id)) {
+        return false;
+    }
+
+    // 字母對應的數字
+    $letters = array(
+        'A' => 10, 'B' => 11, 'C' => 12, 'D' => 13, 'E' => 14,
+        'F' => 15, 'G' => 16, 'H' => 17, 'I' => 34, 'J' => 18,
+        'K' => 19, 'L' => 20, 'M' => 21, 'N' => 22, 'O' => 35,
+        'P' => 23, 'Q' => 24, 'R' => 25, 'S' => 26, 'T' => 27,
+        'U' => 28, 'V' => 29, 'W' => 32, 'X' => 30, 'Y' => 31,
+        'Z' => 33
+    );
+
+    $c1 = $id[0]; // 取得第一個字母
+    $n1 = $letters[$c1]; // 對應的數字
+
+    $n1a = intval($n1 / 10); // 字母數字的十位數
+    $n1b = $n1 % 10; // 字母數字的個位數
+
+    // 權重
+    $weights = array(1, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1);
+
+    // 將所有數字組成陣列
+    $digits = array();
+    $digits[] = $n1a;
+    $digits[] = $n1b;
+    for ($i = 1; $i < 10; $i++) {
+        $digits[] = intval($id[$i]);
+    }
+
+    // 計算總和
+    $checksum = 0;
+    for ($i = 0; $i < 11; $i++) {
+        $checksum += $digits[$i] * $weights[$i];
+    }
+
+    // 檢查總和除以10的餘數是否為0
+    return ($checksum % 10) == 0;
+}
+        if(!validateTaiwanID($ID)){
             echo '<script>alert("Wrong ID")</script>';
             return 0;
         }
